80ad04856c871124629bf39c51e2f42b042c4c33
franklin <etrobot@outlook.com>
2023-12-23 14:41:31 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wechatGemini
+
+### 介绍
+使用itchat-uos将Gemini Pro API接入到微信，当前支持文字聊天和公众号文章总结，未来可能会尝试图片识别。
+
+### 使用
+建立.env文件填入API_KEY='你申请的API_KEY'，运行main.py即可
---
+++ main.py
@@ -0,0 +1,95 @@
+# encoding:utf-8
+import re
+from concurrent.futures import ThreadPoolExecutor
+import itchat
+import requests
+from bs4 import BeautifulSoup
+from itchat.content import *
+import logging
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def ripPost(url:str):
+    response=requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    elements = [
+        element.text for element in soup.find_all(["title", "h1", "h2", "h3", "li", "p", "a"])
+        if len(element.text) > 5
+    ]
+    txt = ' '.join(elements).replace('  ','')
+    if len('\n'.join(txt)) == 0:
+        txt = re.sub(r'\\x[0-9a-fA-F]{2}', '',
+                           soup.find('meta', {'name': 'description'}).attrs['content'])
+    print(txt)
+    return txt
+
+@itchat.msg_register([TEXT, SHARING])
+def handler_single_msg(msg):
+    weChat().handle(msg)
+    return None
+
+@itchat.msg_register([TEXT, SHARING], isGroupChat=True)
+def handler_group_msg(msg):
+    weChat().handle_group(msg)
+    return None
+
+class weChat():
+    def __init__(self):
+        pass
+    def startup(self):
+        # login by scan QRCode
+        itchat.auto_login(hotReload=True)
+        # start message listener
+        itchat.run()
+
+    def handle(self, msg):
+        if msg['MsgType']==49:
+            thread_pool.submit(self._do_send, ripPost(msg['Url'])+'\nTLDR;用中文总结要点', msg['FromUserName'])
+            return
+        if msg['Text'].startswith('！') or msg['Text'].startswith('! '):
+            thread_pool.submit(self._do_send, msg['Text'],msg['FromUserName'])
+
+    def handle_group(self, msg):
+        if msg['MsgType']==49:
+            thread_pool.submit(self._do_send, ripPost(msg['Url'])+'\nTLDR;用中文总结要点', msg['FromUserName'])
+            return
+        elif not msg['IsAt']:
+            return
+        query = msg['Content'][len(msg['ActualNickName']) + 1:]
+        if query is not None:
+            thread_pool.submit(self._do_send_group, query, msg)
+
+    def send(self, msg, receiver):
+        itchat.send(msg, toUserName=receiver)
+
+    def _do_send(self, query,reply_user_id):
+        if query == '':
+            return
+        try:
+            reply_text = self.reply(query)
+            if reply_text is not None:
+                self.send('G:' + reply_text,reply_user_id)
+        except Exception as e:
+            log.exception(e)
+
+    def _do_send_group(self, query, msg):
+        if not query:
+            return
+        group_id = msg['User']['UserName']
+        reply_text = self.reply(query)
+        if reply_text is not None:
+            self.send('@' + msg['ActualNickName'] + ' ' + reply_text.strip(), group_id)
+
+    def reply(self,queryText):
+        return model.generate_content(queryText).text
+
+if __name__=='__main__':
+    load_dotenv(find_dotenv())
+    genai.configure(api_key=os.environ['API_KEY'], transport="rest")
+    model = genai.GenerativeModel('gemini-pro')
+    thread_pool = ThreadPoolExecutor(max_workers=8)
+    log = logging.getLogger('itchat')
+    log.setLevel(logging.DEBUG)
+    wechat = weChat()
+    wechat.startup()
